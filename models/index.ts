89ec27a90df36bb2eb73d7e1a6fd4879b1649c6e
PyiTharTun <pyithartun.may@gmail.com>
2023-12-19 10:44:57 +0630

    secondcommit

--- models/index.ts
+++ models/index.ts
@@ -1,23 +1,29 @@
-const dbConfig = require ("../config/db.config");
+const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize"); // ORM
 
+// const sequelize = new Sequelize(
+//     dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//         host: dbConfig.HOST,
+//         dialect: dbConfig.dialect,
+//         pool:{
+//             max: dbConfig.pool.max,
+//             min: dbConfig.pool.min,
+//             acquire: dbConfig.pool.acquire,
+//             idle: dbConfig.pool.idle
+//         }
+// })
 const sequelize = new Sequelize(
-    dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect,
-        pool:{
-            max: dbConfig.pool.max,
-            min: dbConfig.pool.min,
-            acquire: dbConfig.pool.acquire,
-            idle: dbConfig.pool.idle
-        }
-})
+  "postgres://blogs_e4mt_user:CeHRyyis8ZfLgJixYc16bz1hqcU6WHhR@dpg-cm0gu5a1hbls73daha90-a.singapore-postgres.render.com/blogs_e4mt?ssl=true",
+  {
+    dialect: dbConfig.dialect,
+  }
+);
 
 const db: any = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.blogs = require('./blogs')(sequelize, Sequelize);
-db.users = require('./usermodel')(sequelize,Sequelize);
+db.blogs = require("./blogs")(sequelize, Sequelize);
+db.users = require("./usermodel")(sequelize, Sequelize);
 module.exports = db;
